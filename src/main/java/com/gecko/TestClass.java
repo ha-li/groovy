4acd333b3d4a95a91e03340a4971828a0a1a01f3
ha-li <ha-li@gmail.com>
2016-11-19 13:04:15 -0800

    add a simple java class default stuff

---
+++ src/main/java/com/gecko/TestClass.java
@@ -0,0 +1,10 @@
+package com.gecko;
+
+/**
+ * Created by hlieu on 11/19/16.
+ */
+public class TestClass {
+    public static void main (String[] args) {
+        System.out.println("Welcome to Groovy");
+    }
+}
