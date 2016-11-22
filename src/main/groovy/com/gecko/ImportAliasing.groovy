f7b547b2a4fc3e34a5beee5bddff278a0e9528f6
ha-li <ha-li@gmail.com>
2016-11-21 21:37:06 -0800

    Aliasing example at work

---
+++ src/main/groovy/com/gecko/ImportAliasing.groovy
@@ -0,0 +1,20 @@
+package com.gecko
+
+
+// this does an import with an alias
+import java.math.MathContext as MC
+
+
+/**
+ * Created by hlieu on 11/21/16.
+ */
+class ImportAliasing {
+    public static void main (String[] args) {
+        double d = -12.03;
+
+        // here we use the alias of the import
+        MC mc = new MC(4);
+        int e = mc.getPrecision();
+        assert e == 4;
+    }
+}
